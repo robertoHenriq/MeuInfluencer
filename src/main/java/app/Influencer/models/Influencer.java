5b23df8a73a696161faa55a7e0efe1e2911173ea
robertoHenriq <newrobertsilva@gmail.com>
2026-04-04 23:20:54 -0300

    Influencer está sendo criado e inserido no banco corretamente, o front está listando e cadastrando corretamnete os influencers

--- src/main/java/app/Influencer/models/Influencer.java
+++ src/main/java/app/Influencer/models/Influencer.java
@@ -3,6 +3,7 @@ package app.Influencer.models;
 import app.RedeSocial;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -11,39 +12,64 @@ public class Influencer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
-    private int quantidadeDeSeguidoresTotal;
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "Influencer_id")
-    private List<RedeSocial> redesSociais;
+    private int seguidores;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "influencer_id")
+    private List<RedeSocial> redesSociais = new ArrayList<>();
 
-    public String cadastrarNome(String nome){
-        return this.nome = nome;
+    public Influencer(Influencer influencer){
+        this.id = influencer.getId();
+        this.nome = influencer.getNome();
+        this.redesSociais = influencer.redesSociais;
+        this.seguidores = influencer.getSeguidores()    ;
+    }
+
+    public Influencer() {
+
     }
 
     public List<RedeSocial> cadastrarRedeSocial(String nome, int quantidadeDeSeguidores, String linkDoPerfil ) {
         RedeSocial redeSocialNova = new RedeSocial(nome, quantidadeDeSeguidores, linkDoPerfil);
         this.redesSociais.add(redeSocialNova);
-        this.quantidadeDeSeguidoresTotal += quantidadeDeSeguidores;
+        this.seguidores += quantidadeDeSeguidores;
         return this.redesSociais;
+    }
+
+    public void atualizarTotalSeguidores() {
+        this.seguidores = redesSociais.stream()
+                .mapToInt(RedeSocial::getQuantidadeDeSeguidores)
+                .sum();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public int getQuantidadeDeSeguidoresTotal() {
-        return quantidadeDeSeguidoresTotal;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public List<RedeSocial> removerRedeSocialPeloNome(String nome) {
-        for(RedeSocial redeSocial: this.redesSociais){
-            if(redeSocial.getNome().equals(nome)){
-                this.redesSociais.remove(redeSocial);
-                return this.redesSociais;
-            }else {
-                return null;
-            }
-        }
-        return null;
+    public int getSeguidores() {
+        return seguidores;
+    }
+
+    public void setSeguidores(int seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    public List<RedeSocial> getRedesSociais() {
+        return redesSociais;
+    }
+
+    public void setRedesSociais(List<RedeSocial> redesSociais) {
+        this.redesSociais = redesSociais;
     }
 }
--- src/main/java/app/Influencer/services/InfluencerService.java
+++ src/main/java/app/Influencer/services/InfluencerService.java
@@ -4,6 +4,7 @@ import app.Influencer.models.Influencer;
 import app.Influencer.repositorys.InfluencerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,4 +24,16 @@ public class InfluencerService {
         influencerRepository.save(influencer);
     }
 
+    public Influencer editarInfluencer(Influencer i){
+        if(influencerRepository.existsById(i.getId())){
+            influencerRepository.deleteById(i.getId());
+            adicionar(i);
+        }
+        return i;
+    }
+
+    public void removerInfluencer(Integer i){
+        influencerRepository.deleteById(i);
+
+    }
 }
